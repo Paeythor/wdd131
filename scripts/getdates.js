c09f92b2e828560929e3105819d9cbe7a482f94a
Paeythor <theschumann@byui.edu>
2025-03-11 21:51:23 -0600

    test

---
+++ scripts/getdates.js
@@ -0,0 +1,14 @@
+
+
+
+getdates.js 
+document.addEventListener("DOMContentLoaded", function () {
+    const currentYearSpan = document.getElementById("currentyear");
+    const lastModifiedParagraph = document.getElementById("lastModified");
+
+    const currentYear = new Date().getFullYear();
+    currentYearSpan.textContent = currentYear;
+
+    const lastModifiedDate = new Date(document.lastModified);
+    lastModifiedParagraph.textContent = LastModifiedDate; ${lastModifiedDate.toLocaleString()};
+});
