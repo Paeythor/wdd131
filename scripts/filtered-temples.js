6120c725bce52e026f8bff6de44fb5382cb65cc3
Paeythor <theschumann@byui.edu>
2025-04-02 19:34:24 -0600

    fixing missing javascript from previous edit

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -1,5 +1,29 @@
-const temples = [
-  {
+document.getElementById("lastModified").innerHTML = document.lastModified;
+// use the date object
+const today = new Date();
+const year = document.querySelector("#currentyear");
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+
+
+
+const hamburgerBtn = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamburgerBtn.addEventListener('click', () => {
+    hamburgerBtn.classList.toggle('open');
+    navigation.classList.toggle('open');
+
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const imageContainer = document.getElementById('imageContainer');
+    // const cardContainer = document.getElementById('cardContainer');
+    const mainContainer = document.getElementById('mainContainer');
+
+    var temples = [
+        {
       templeName: "Aba Nigeria",
       location: "Aba, Nigeria",
       dedicated: "2005, August, 7",
