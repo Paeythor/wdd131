485944a6c07fa5614416e9913d21379bfdbf77e8
Paeythor <theschumann@byui.edu>
2025-04-08 12:37:18 -0600

    additions

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -11,72 +11,207 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Get the temple data
     const temples = [
+            {
+                templeName: "Aba Nigeria",
+                location: "Aba, Nigeria",
+                dedicated: "2005, August, 7",
+                area: 11500,
+                imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/aba-nigeria-temple/aba-nigeria-temple-5087-main.jpg",
+                templeUrl: "https://churchofjesuschristtemples.org/aba-nigeria-temple/",
+                galleryURL: "https://churchofjesuschristtemples.org/aba-nigeria-temple/photographs/",
+                dedicatedYear: 2005
+            },
+            {
+                templeName: "Auckland New Zealand",
+                location: "Auckland, New Zealand",
+                dedicated: "2025, April, 13",
+                area: 45456,
+                imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/auckland-new-zealand-temple/auckland-new-zealand-temple-56277-main.jpg",
+                templeUrl: "https://churchofjesuschristtemples.org/auckland-new-zealand-temple/",
+                galleryURL: "https://churchofjesuschristtemples.org/auckland-new-zealand-temple/photographs/",
+                dedicatedYear: 2025
+            },
+            {
+                templeName: "Laie Hawaii",
+                location: "Laie, Hawaii, USA",
+                dedicated: "1919, November, 27",
+                area: 42100,
+                imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/laie-hawaii-temple/laie-hawaii-temple-7370-main.jpg",
+                templeUrl: "https://churchofjesuschristtemples.org/laie-hawaii-temple/",
+                galleryURL: "https://churchofjesuschristtemples.org/laie-hawaii-temple/photographs/",
+                dedicatedYear: 1919
+            },
+            {
+                templeName: "Lima Peru",
+                location: "Lima, Peru",
+                dedicated: "1986, January, 10",
+                area: 9600,
+                imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/lima-peru-temple/lima-peru-temple-12721-main.jpg",
+                templeUrl: "https://churchofjesuschristtemples.org/lima-peru-temple/",
+                galleryURL: "https://churchofjesuschristtemples.org/lima-peru-temple/photographs/",
+                dedicatedYear: 1986
+            },
+            {
+                templeName: "Manti Utah",
+                location: "Manti, Utah, USA",
+                dedicated: "1888, May, 21",
+                area: 35000,
+                imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/manti-utah-temple/manti-utah-temple-40551-main.jpg",
+                templeUrl: "https://churchofjesuschristtemples.org/manti-utah-temple/",
+                galleryURL: "https://churchofjesuschristtemples.org/manti-utah-temple/photographs/",
+                dedicatedYear: 1888
+            },
+            {
+                templeName: "Mexico City Benemerito",
+                location: "Mexico City, Mexico",
+                dedicated: "2000, December, 2",
+                area: 20000,
+                imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/mexico-city-benemerito-mexico-temple/mexico-city-benemerito-mexico-temple-56024-main.jpg",
+                templeUrl: "https://churchofjesuschristtemples.org/mexico-city-benemerito-mexico-temple/",
+                galleryURL: "https://churchofjesuschristtemples.org/mexico-city-benemerito-mexico-temple/photographs/",
+                dedicatedYear: 2000
+            },
+            {
+                templeName: "Mexico City Mexico",
+                location: "Mexico City, Mexico",
+                dedicated: "2000, December, 1",
+                area: 25000,
+                imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/mexico-city-mexico-temple/mexico-city-mexico-temple-4060-main.jpg",
+                templeUrl: "https://churchofjesuschristtemples.org/mexico-city-mexico-temple/",
+                galleryURL: "https://churchofjesuschristtemples.org/mexico-city-mexico-temple/photographs/",
+                dedicatedYear: 2000
+            },
+            {
+                templeName: "Mount Timpanogos Utah",
+                location: "American Fork, Utah, USA",
+                dedicated: "1996, October, 9",
+                area: 27000,
+                imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/mount-timpanogos-utah-temple/mount-timpanogos-utah-temple-36979-main.jpg",
+                templeUrl: "https://churchofjesuschristtemples.org/mount-timpanogos-utah-temple/",
+                galleryURL: "https://churchofjesuschristtemples.org/mount-timpanogos-utah-temple/photographs/",
+                dedicatedYear: 1996
+            },
+            {
+                templeName: "Nauvoo Illinois",
+                location: "Nauvoo, Illinois, USA",
+                dedicated: "2002, May, 1",
+                area: 35000,
+                imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/nauvoo-illinois-temple/nauvoo-illinois-temple-50576-main.jpg",
+                templeUrl: "https://churchofjesuschristtemples.org/nauvoo-illinois-temple/",
+                galleryURL: "https://churchofjesuschristtemples.org/nauvoo-illinois-temple/photographs/",
+                dedicatedYear: 2002
+            },
+            {
+                templeName: "Oakland California",
+                location: "Oakland, California, USA",
+                dedicated: "1964, November, 23",
+                area: 28000,
+                imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/oakland-california-temple/oakland-california-temple-2654-main.jpg",
+                templeUrl: "https://churchofjesuschristtemples.org/oakland-california-temple/",
+                galleryURL: "https://churchofjesuschristtemples.org/oakland-california-temple/photographs/",
+                dedicatedYear: 1964
+            },
+            {
+                templeName: "Orem Utah",
+                location: "Orem, Utah, USA",
+                dedicated: "2021, March, 20",
+                area: 21000,
+                imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/orem-utah-temple/orem-utah-temple-39549-main.jpg",
+                templeUrl: "https://churchofjesuschristtemples.org/orem-utah-temple/",
+                galleryURL: "https://churchofjesuschristtemples.org/orem-utah-temple/photographs/",
+                dedicatedYear: 2021
+            },
+            {
+                templeName: "Payson Utah",
+                location: "Payson, Utah, USA",
+                dedicated: "2015, April, 5",
+                area: 23000,
+                imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/payson-utah-temple/payson-utah-temple-38451-main.jpg",
+                templeUrl: "https://churchofjesuschristtemples.org/payson-utah-temple/",
+                galleryURL: "https://churchofjesuschristtemples.org/payson-utah-temple/photographs/",
+                dedicatedYear: 2015
+            },
+            {
+                templeName: "Phoenix Arizona",
+                location: "Phoenix, Arizona, USA",
+                dedicated: "2015, November, 18",
+                area: 19000,
+                imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/phoenix-arizona-temple/phoenix-arizona-temple-12711-main.jpg",
+                templeUrl: "https://churchofjesuschristtemples.org/phoenix-arizona-temple/",
+                galleryURL: "https://churchofjesuschristtemples.org/phoenix-arizona-temple/photographs/",
+                dedicatedYear: 2015
+            },
+            {
+                templeName: "Provo City Center Utah",
+                location: "Provo, Utah, USA",
+                dedicated: "2016, March, 20",
+                area: 25000,
+                imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/provo-city-center-temple/provo-city-center-temple-56386-main.jpg",
+                templeUrl: "https://churchofjesuschristtemples.org/provo-city-center-temple/",
+                galleryURL: "https://churchofjesuschristtemples.org/provo-city-center-temple/photographs/",
+                dedicatedYear: 2016
+            },
+            {
+                templeName: "Provo Rock Canyon Utah",
+                location: "Provo, Utah, USA",
+                dedicated: "To be announced",
+                area: 23000,
+                imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/provo-utah-rock-canyon-temple/provo-utah-rock-canyon-temple-45659-main.jpg",
+                templeUrl: "https://churchofjesuschristtemples.org/provo-utah-rock-canyon-temple/",
+                galleryURL: "https://churchofjesuschristtemples.org/provo-utah-rock-canyon-temple/photographs/",
+                dedicatedYear: null
+            },
+            {
+                templeName: "Querétaro Mexico",
+                location: "Querétaro, Querétaro, Mexico",
+                dedicated: "To be announced",
+                area: 27500,
+                imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/queretaro-mexico-temple/queretaro-mexico-temple-23845.jpg",
+                templeUrl: "https://churchofjesuschristtemples.org/queretaro-mexico-temple/",
+                galleryURL: "https://churchofjesuschristtemples.org/queretaro-mexico-temple/photographs/",
+                dedicatedYear: null
+            },
+            {
+                templeName: "Rome Italy Temple",
+                location: "Rome, Italy",
+                dedicated: "2019, March, 12",
+                area: 41010,
+                imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/rome-italy-temple/rome-italy-temple-2642-main.jpg",
+                templeUrl: "https://churchofjesuschristtemples.org/rome-italy-temple/",
+                galleryURL: "https://churchofjesuschristtemples.org/rome-italy-temple/photographs/",
+                dedicatedYear: 2019
+            },
+            {
+                templeName: "San Diego California",
+                location: "San Diego, California, USA",
+                dedicated: "2002, January, 1",
+                area: 28000,
+                imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/san-diego-california-temple/san-diego-california-temple-9060-main.jpg",
+                templeUrl: "https://churchofjesuschristtemples.org/san-diego-california-temple/",
+                galleryURL: "https://churchofjesuschristtemples.org/san-diego-california-temple/photographs/",
+                dedicatedYear: 2002
+            },
         {
-            templeName: "Aba Nigeria",
-            location: "Aba, Nigeria",
-            dedicated: "2005, August, 7",
-            area: 11500,
-            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg",
-            dedicatedYear: 2005
+            templeName: "Washington DC",
+            location: "Washington, D.C., USA",
+            dedicated: "1975, November, 19",
+            area: 32000,
+            imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/washington-d.c.-temple/washington-d.c.-temple-14992-main.jpg",
+            templeUrl: "https://churchofjesuschristtemples.org/washington-d.c.-temple/",
+            galleryURL: "https://churchofjesuschristtemples.org/washington-d.c.-temple/photographs/",
+            dedicatedYear: 1975
         },
         {
-            templeName: "Manti Utah",
-            location: "Manti, Utah, United States",
-            dedicated: "1888, May, 21",
-            area: 74792,
-            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg",
-            dedicatedYear: 1888
-        },
-        {
-            templeName: "Payson Utah",
-            location: "Payson, Utah, United States",
-            dedicated: "2015, June, 7",
-            area: 96630,
-            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg",
-            dedicatedYear: 2015
-        },
-        {
-            templeName: "Washington D.C.",
-            location: "Kensington, Maryland, United States",
-            dedicated: "1974, November, 19",
-            area: 156558,
-            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg",
-            dedicatedYear: 1974
-        },
-        {
-            templeName: "Lima Perú",
-            location: "Lima, Perú",
-            dedicated: "1986, January, 10",
-            area: 9600,
-            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg",
-            dedicatedYear: 1986
-        },
-        {
-            templeName: "Mexico City Mexico",
-            location: "Mexico City, Mexico",
-            dedicated: "1983, December, 2",
-            area: 116642,
-            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg",
-            dedicatedYear: 1983
-        },
-        {
-            templeName: "Phoenix Arizona",
-            location: "Phoenix, Arizona, United States",
-            dedicated: "2014, November, 16",
-            area: 27000,
-            imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/phoenix-arizona-temple/phoenix-arizona-temple-36265.jpg",
-            dedicatedYear: 2014
-        },
-        {
-            templeName: "Querétaro Mexico",
-            location: "Querétaro, Querétaro, Mexico",
-            dedicated: "To be announced",
-            area: 27500,
-            imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/queretaro-mexico-temple/queretaro-mexico-temple-23845.jpg",
-            dedicatedYear: null
+            templeName: "Yigo Guam",
+            location: "Yigo, Guam",
+            dedicated: "2020, May, 2",
+            area: 6861,
+            imageUrl:"https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg",
+            templeURL:"https://churchofjesuschristtemples.org/yigo-guam-temple/",
+            galleryURL:"https://churchofjesuschristtemples.org/yigo-guam-temple/photographs/",
+            dedicatedYear: 2020
         }
     ];
-
     // Function to create and display temple cards
     function createTempleCard(filteredTemples) {
         const gallery = document.getElementById('temple-gallery');
