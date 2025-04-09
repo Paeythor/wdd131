ecfa45b4f15f4c9e120c21ced47bd040d93ff4a7
Paeythor <theschumann@byui.edu>
2025-04-09 17:24:28 -0600

    more work

---
+++ Project/scripts/wot.js
@@ -0,0 +1,48 @@
+{
+    BookTitle: "Towers of Midnight",
+    BookOrder: "13",
+    Author: "Robert Jordan / Brandon Sanderson",
+    releaseDate: "2020",
+    numberofpages: 863,
+    BookCover: "images/WoT-Books/WoT13_TowersOfMidnight.jpg"
+},
+{
+    BookTitle: "Towers of Midnight",
+    BookOrder: "13",
+    Author: "Robert Jordan / Brandon Sanderson",
+    releaseDate: "2020",
+    numberofpages: 863,
+    BookCover: "images/WoT-Books/WoT13_TowersOfMidnight.jpg"
+},
+{
+    BookTitle: "Towers of Midnight",
+    BookOrder: "13",
+    Author: "Robert Jordan / Brandon Sanderson",
+    releaseDate: "2020",
+    numberofpages: 863,
+    BookCover: "images/WoT-Books/WoT13_TowersOfMidnight.jpg",
+},
+{
+    BookTitle: "TheGatheringStorm",
+    BookOrder: "12",
+    Author: "Robert Jordan / Brandon Sanderson",
+    releaseDate: "2009",
+    numberofpages: 784,
+    BookCover: "images/WoT-Books/WoT12_TheGatheringStorm.jpg"
+},
+{
+    BookTitle: "Towers of Midnight",
+    BookOrder: "13",
+    Author: "Robert Jordan / Brandon Sanderson",
+    releaseDate: "2020",
+    numberofpages: 863,
+    BookCover: "images/WoT-Books/WoT13_TowersOfMidnight.jpg"
+},
+{
+    BookTitle: "A Memory of light",
+    BookOrder: "14",
+    Author: "Robert Jordan / Brandon Sanderson",
+    releaseDate: "2013",
+    numberofpages: 912,
+    BookCover: "images/WoT-Books/WoT14_MemoryOfLight.jpg"
+},
